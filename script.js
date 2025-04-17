1655b0733035da2d880b610a6a71d1c7c61e8d5b
Scott <scottrpileggi@gmail.com>
2025-04-17 02:17:04 -0500

    make 'back-to-top' button display: none when back to top of page

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const backToTopButton = document.getElementById("back-to-top");
+
+// Function to toggle the visibility of the button
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    // Show the button when scrolled down 100px
+    backToTopButton.style.opacity = "1";
+    backToTopButton.style.visibility = "visible";
+  } else {
+    // Hide the button when at the top
+    backToTopButton.style.opacity = "0";
+    backToTopButton.style.visibility = "hidden";
+  }
+});
+
 function scrollToTop() {
   window.scrollTo({
     top: 0,
